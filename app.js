39b0764f0434aae8c9e9a27eee5ce41df9446b7c
giba <gibim6@gmail.com>
2019-12-01 14:29:38 +0000

    Using puppeteer-cluster YOLOOO

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const puppeteer = require('puppeteer');
 const _ = require('lodash');
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3001
+const express = require('express');
+const { Cluster } = require('puppeteer-cluster');
+const app = express();
+const port = process.env.PORT || 3001;
 
 const desired_areas = [
   'dublin-4',
   'dublin-6',
   'dublin-6w',
   'dublin-7',
-  // 'dublin-8',
-  // 'dublin-14',
-  // 'sandyford',
-  // 'rathmines',
+  'dublin-8',
+  'dublin-14',
+  'sandyford',
+  'rathmines',
   'ballsbridge'] // Desired areas in dublin, matching daft.ie url
 const maximum_price_per_bedroom = 800 // The maximum price you wish to pay per room
 
 function remove_nondigits(str) { return Number(str.replace(/\D/g,'')) }
 
 function build_urls(areas) {
-  desired_rooms_qty = [3]
+  desired_rooms_qty = [2, 3]
 
   urls = areas.map(area => desired_rooms_qty.map(i => ({
     "area": area,
@@ -56,6 +57,30 @@ async function fetch_ads(browser, area_url) {
   return ads
 }
 
+async function clustered_fetch_ads({ page, data: area_url }) {
+  await page.goto(area_url.url, {timeout: 0});
+  const ad_urls = await page.$$eval('.sr_counter + a', ads => ads.map(ad => ad.href));
+  const addresses = await page.$$eval('.sr_counter + a', ads => ads.map(ad => ad.innerText));
+  const prices = await page.$$eval('.price', ads => ads.map(ad => ad.innerText));
+  const bedrooms = await page.$$eval('.info li:nth-child(2)', ads => ads.map(ad => ad.innerText));
+  const images = await page.$$eval('.box .main_photo', ads => ads.map(ad => ad.src));
+  
+  ads = ad_urls.map((ad_url, index) => {
+    bedrooms_qty = remove_nondigits(bedrooms[index])
+    return {
+      "area": area_url.area,
+      "address": addresses[index],
+      "ad_url": ad_url,
+      "image": images[index],
+      "bedrooms_qty": remove_nondigits(bedrooms[index]),
+      "price_per_room": remove_nondigits(prices[index]) / bedrooms_qty,
+      "total_price": prices[index]
+    }
+  })
+
+  return ads;
+}
+
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -63,20 +88,22 @@ app.use(function(req, res, next) {
 });
 
 app.get('/api', (req, res) => {
-  areas = req.query.areas
-  console.log(areas)
-  puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
-    .then(browser => {
-      Promise.all(
-        build_urls(desired_areas).map(url => { return fetch_ads(browser, url) })
-      ).then(ads => _.flatten(ads))
-       .then(ads => _.uniqBy(ads, (ad => ad.ad_url)))
-       .then(ads => _.sortBy(ads, [ad => ad.price_per_room]))
-       .then(ads => {
-         browser.close;
-         return res.send(ads);
-       })
-       .catch(console.log)
+  urls = build_urls(desired_areas)
+
+  Cluster.launch({ concurrency: Cluster.CONCURRENCY_CONTEXT, maxConcurrency: 2 })
+    .then(cluster => {
+      cluster.task(clustered_fetch_ads)
+        .then(() => Promise.all(urls.map(url => cluster.execute(url))))
+        .then(ads => {
+          cluster.idle().then(() => cluster.close())
+          return _.flatten(ads)
+        })
+        .then(ads => _.uniqBy(ads, (ad => ad.ad_url)))
+        .then(ads => _.sortBy(ads, [ad => ad.price_per_room]))
+        .then(ads => {
+          return res.send(ads);
+        })
+      .catch(console.log)
     })
 })
 
